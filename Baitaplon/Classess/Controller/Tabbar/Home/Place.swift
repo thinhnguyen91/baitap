c16183ba0ae7362139a1a5e5df2158bc06806560
ThinhNX <xuanthinh.pn@gmail.com>
2016-05-10 17:27:58 +0700

    map pin

---
+++ Baitaplon/Classess/Controller/Tabbar/Home/Place.swift
@@ -0,0 +1,35 @@
+//
+//  List.swift
+//  Baitaplon
+//
+//  Created by ThinhNX on 4/27/16.
+//  Copyright © 2016 AsianTech. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class Place: NSObject, MKAnnotation{
+    var name_: String = ""
+    var avatar_: String = ""
+    var addess_: String = ""
+    var start_: String = ""
+    var image: UIImage?
+    let title: String?
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+
+    var subtitle: String? {
+        return locationName
+    }
+}
